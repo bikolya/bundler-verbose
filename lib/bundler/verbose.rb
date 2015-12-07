d6db3a8024a3799dce31a40d6618ad2084b29f4f
Nikolay Bienko <bikolya@gmail.com>
2015-12-07 21:02:21 +0300

    Initial commit

---
+++ lib/bundler/verbose.rb
@@ -0,0 +1,7 @@
+require "bundler/verbose/version"
+
+module Bundler
+  module Verbose
+    # Your code goes here...
+  end
+end
---
+++ lib/bundler/verbose/version.rb
@@ -0,0 +1,5 @@
+module Bundler
+  module Verbose
+    VERSION = "0.1.0"
+  end
+end
